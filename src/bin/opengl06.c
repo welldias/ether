06589d925de5455346b9af10978f58578785c5d2
Well Dias <welldias@yahoo.com>
2020-07-21 01:24:16 -0300

    WIP

--- src/bin/opengl06.c
+++ src/bin/opengl06.c
@@ -6,11 +6,25 @@
 #include <ether_glfw_driver.h>
 #include <stdio.h>
 
+#define VEC3_BACK     ((vec3){0,  0, -1})
+#define VEC3_DOWN     ((vec3){0, -1,  0})
+#define VEC3_FORWARD  ((vec3){0,  0,  1})
+#define VEC3_LEFT     ((vec3){-1, 0,  0})
+#define VEC3_RIGHT    ((vec3){1,  0,  0})
+#define VEC3_UP       ((vec3){0,  1,  0})
+
+int global_totalVestices = 0;
+int global_totalTriangles = 0;
+
+float* global_vertices = NULL;
+unsigned int* global_triangles = NULL;
+
 /* converting rgb color to opengl format */
 //float out = (1.0f / 255) * byte_in;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
+void build_planet_mash(int resolution, vec3 localUp);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
@@ -104,6 +118,7 @@ int main()
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
+#if FALSE
     float vertices[] = {
          0.5f,  0.5f, 0.0f,  // top right
          0.5f, -0.5f, 0.0f,  // bottom right
@@ -115,6 +130,37 @@ int main()
         1, 2, 3   // second Triangle
     };
 
+    size_t countVertices = sizeof(vertices);
+    size_t countIndices = sizeof(indices);
+
+#else
+    build_planet_mash(4, VEC3_FORWARD);
+    
+    size_t countVertices = sizeof(float) * global_totalVestices * 3;
+    size_t countIndices  = sizeof(int) * global_totalTriangles;
+
+    float * vertices = global_vertices;
+    unsigned int* indices = global_triangles;
+
+    /*
+    {
+        int i = 0;
+        int x = 0;
+        for (x = 0; x < global_totalVestices; x++)
+        {
+            i = x * 3;
+            printf("x:%02.02f y:%02.02f z:%02.02f\n", *(vertices + i), *(vertices + i + 1), *(vertices + i + 2));
+        }
+
+        for (x = 0; x < (global_totalTriangles/3); x++)
+        {
+            i = x * 3;
+            printf("i:%d i:%d i:%d\n", *(indices + i), *(indices + i + 1), *(indices + i + 2));
+        }
+
+    }
+    */
+#endif
 
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
@@ -124,10 +170,10 @@ int main()
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, countVertices, vertices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, countIndices, indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -200,4 +246,62 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
+}
+
+void build_planet_mash(int resolution, vec3 localUp)
+{
+    global_totalVestices = resolution * resolution;
+    global_totalTriangles = (resolution - 1) * (resolution - 1) * 6;
+
+    global_vertices = (float*)malloc(sizeof(float) * global_totalVestices * 3);
+    global_triangles = (unsigned int*)malloc(sizeof(int) * global_totalTriangles);
+
+    int i = 0;
+    int t = 0;
+
+    //vec3 axisA = { localUp[1], localUp[2], localUp[0] };
+    vec3 axisA = { localUp[1], localUp[2], localUp[0] };
+    vec3 axisB;
+
+    glm_vec3_cross(localUp, axisA, axisB);
+
+    vec2 percent;
+    vec3 aux;
+    vec3 pointOnUnitCube;
+
+    for (int y = 0; y < resolution; y++) {
+        for (int x = 0; x < resolution; x++) {
+            i = x + y * resolution;
+
+            glm_vec3_zero(pointOnUnitCube);
+            glm_vec3_zero(aux);
+
+            percent[0] = (float)x / (resolution - 1);
+            percent[1] = (float)y / (resolution - 1);
+
+            glm_vec3_scale(axisA, ((percent[0] - 0.5f) * 2), aux);
+            glm_vec3_add(aux, pointOnUnitCube, pointOnUnitCube);
+
+            glm_vec3_scale(axisB, ((percent[1] - 0.5f) * 2), aux);
+            glm_vec3_add(aux, pointOnUnitCube, pointOnUnitCube);
+
+            glm_vec3_add(localUp, pointOnUnitCube, pointOnUnitCube);
+            //glm_vec3_normalize(pointOnUnitCube);
+
+            glm_vec3_copy(pointOnUnitCube, global_vertices + (i * 3));
+
+            if (x != resolution - 1 && y != resolution - 1)
+            {
+                global_triangles[t] = i;
+                global_triangles[t + 1] = i + resolution + 1;
+                global_triangles[t + 2] = i + resolution;
+
+                global_triangles[t + 3] = i;
+                global_triangles[t + 4] = i + 1;
+                global_triangles[t + 5] = i + resolution + 1;
+
+                t += 6;
+            }
+        }
+    }
 }
