abf8fac4d275160b01c08e11b21ba5cf768cad4f
Well Dias <welligton.dias@linx.com.br>
2020-07-21 01:24:16 -0300

    WIP

--- src/bin/opengl05.c
+++ src/bin/opengl05.c
@@ -5,15 +5,16 @@
 #include <Ether.h>
 #include <ether_glfw_driver.h>
 
-/* converting rgb color to opengl format */
-//float out = (1.0f / 255) * byte_in;
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+float move_cam_x = 0.0f;
+float move_cam_y = 0.0f;
+float move_cam_z = 0.0f;
 
 int main()
 {
@@ -51,7 +52,7 @@ int main()
     // build and compile our shader program
     // ------------------------------------
     EtherShader shaderId = 0;
-    if (ether_shader_load("src/bin/3.3.shader.vs", "src/bin/3.3.shader.fs", &shaderId) != 0)
+    if (ether_shader_load("src/bin/opengl05.vs", "src/bin/opengl05.fs", &shaderId) != 0)
     {
         printf("Failed to load and compile shaders\n");
         return -1;
@@ -60,32 +61,50 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        // positions         // colors
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+        // positions          // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   0.0f, 1.0f  // top left 
     };
-
-    unsigned int VBO, VAO;
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
     // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    // texture coord attribute
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    EtherTexture textureId = 0;
+    if (ether_texture_load("src/bin/container.jpg", &textureId) != 0)
+    {
+        printf("Failed to load texture\n");
+        return -1;
+    }
+
+    ether_shader_use(shaderId);
+    ether_shader_uniform_integer(shaderId, "texture1", 0);
+
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     // glBindVertexArray(0);
-
 
     // render loop
     // -----------
@@ -100,10 +119,34 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // render the triangle
-        ether_shader_use(shaderId);
+        // bind Texture
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, textureId);
+
+        mat4 model      = GLM_MAT4_IDENTITY_INIT;
+        mat4 view       = GLM_MAT4_IDENTITY_INIT;
+        mat4 projection = GLM_MAT4_IDENTITY_INIT;
+
+        vec3 v1; v1[0] = 1.0f; v1[1] = 0.0f; v1[2] = 0.0f;
+        vec3 v2; v2[0] = 0.0f + move_cam_x; v2[1] = 0.0f + move_cam_y; v2[2] = -3.0f + move_cam_z;
+
+        glm_rotate(model, (float)glfwGetTime() , v1);
+        glm_translate(view, v2);
+        glm_perspective_default((float)SCR_WIDTH / (float)SCR_HEIGHT, projection);
+
+        // retrieve the matrix uniform locations
+        unsigned int modelLoc = glGetUniformLocation(shaderId, "model");
+        unsigned int viewLoc = glGetUniformLocation(shaderId, "view");
+
+        // pass them to the shaders (3 different ways)
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, (GLfloat *)model);
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, (GLfloat*)view);
+        // note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
+        ether_shader_uniform_mat4(shaderId, "projection", projection);
+
+        // render container
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
@@ -115,8 +158,10 @@ int main()
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
 
     ether_shader_unload(shaderId);
+    ether_texture_unload(textureId);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -130,6 +175,18 @@ void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+        move_cam_z -= 0.1f;
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+        move_cam_z += 0.1f;
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+        move_cam_x -= 0.1f;
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+        move_cam_x += 0.1f;
+    if (glfwGetKey(window, GLFW_KEY_PAGE_DOWN) == GLFW_PRESS)
+        move_cam_y -= 0.1f;
+    if (glfwGetKey(window, GLFW_KEY_PAGE_UP) == GLFW_PRESS)
+        move_cam_y += 0.1f;
 }
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
