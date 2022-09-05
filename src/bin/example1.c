18cc4181e03895b1fd008e4976ab1fdbd21faf53
Well Dias <welldias@yahoo.com>
2022-09-05 19:51:35 -0300

    arilgpl.zip added

--- src/bin/example1.c
+++ src/bin/example1.c
@@ -10,13 +10,21 @@ int main(int argc, char **argv)
   EtherObject *cube = NULL;
   EtherCamera *camera = NULL;
   EtherLight  *light = NULL;
+  //EtherSurfaceMap *map = NULL;
 
-  //ether_system_blit_func_set(ether_blit);
+  // ether_system_blit_func_set(ether_blit);
   ether_system_startup();
   //ether_render_draw_mode_set(1);
 
-  cube = ether_object_create(ether_primitives_box(100, 100, 100, NULL));
-  ether_object_rot_y(cube, ETHER_FLOAT_TO_ANGLE(45));
+  cube = ether_object_create(ether_primitives_cylinder(50, 50, 100, 50, NULL));
+  //cube = ether_object_create(ether_primitives_box(100, 100, 100, NULL));
+  // cube = ether_object_create(ether_primitives_sphere(50, 15, 15, NULL));
+  // ether_object_rot_y(cube, ETHER_FLOAT_TO_ANGLE(45));
+
+  // ether_object_surfacemap_set(cube, ether_surface_map_create(1));
+  // map = ether_object_surfacemap_get(cube);
+  // ether_surface_map_surface_set(map, 1, ether_surface_create(1));
+
   ether_coloring(cube);
 
   light = ether_light_create();
@@ -36,7 +44,9 @@ int main(int argc, char **argv)
 void ether_rot_obj(void)
 {
   EtherObject *obj = (EtherObject*)ether_task_data_get();
-  ether_object_rot_y(obj, 0.1);
+  ether_object_rot_x(obj, 0.3);
+  ether_object_rot_y(obj, 0.3);
+  ether_object_rot_z(obj, 0.3);
   ether_system_request_refresh();
 }
 
@@ -45,11 +55,13 @@ void ether_coloring(EtherObject* obj)
   int x;
   EtherSurface *surf;
   EtherSurfaceMap *map = ether_object_surfacemap_get(obj);
+
   int count = ether_surface_map_count_entries(map);
 
   for(x=0;x<count;x++)
   {
     surf = ether_surface_map_surface_get(map,x);
-    ether_surface_hue_set(surf, x+1);
+    ether_surface_type_set(surf, ETHER_SURF_SIMPLE);
+    ether_surface_hue_set(surf, x + 1);
   }
 }
